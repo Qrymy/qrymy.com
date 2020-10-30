0994efddc943bcf63589ecfbfbf2a9619e9eb5e3
Qrymy <regist@qry.mx>
2020-10-30 21:40:18 +0900

    Create header navigation items

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const DIRECTORY_NAME = '_articles' as const
 
 export const HEADER = [
-  { label: 'qrymy.com', href: 'https://qrymy.com' },
+  { label: 'qrymy.com', href: '/' },
+  { label: '詳細', href: '/about' },
 ] as const
