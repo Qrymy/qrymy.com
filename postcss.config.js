f0cc590999dbce4a875e251d9b38bda83f5332f2
Qrymy <regist@qry.mx>
2020-12-06 20:36:06 +0900

    Install tailwindcss, create config files

---
+++ postcss.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  plugins: ['taiwindcss', 'postcss-preset-env'],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
