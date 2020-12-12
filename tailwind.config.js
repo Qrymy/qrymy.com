979e4cbb3562f303ac9410ee2086fafbb12d3157
Qrymy <regist@qry.mx>
2020-12-12 13:15:43 +0900

    Extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@ module.exports = {
   purge: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
   darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        DEFAULT: '#243FFE',
+      },
+      secondary: {
+        DEFAULT: '#E71D36',
+      },
+    },
+    extend: {
+      colors: {
+        gray: {
+          50: '#F8F8FF',
+          200: '#ECEBF3',
+          900: '#080708',
+        },
+      },
+      lineHeight: {
+        14: '3.5rem',
+      },
+    },
   },
   variants: {
     extend: {},
