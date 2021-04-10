8afe16c05fea164676351f882bd4646c8d6b386a
Qrymy <regist@qry.mx>
2021-04-10 22:21:54 +0900

    ini new post

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,29 +1,11 @@
 import { FC } from 'react'
-import MarkdownToJSX, { MarkdownToJSX as MarkdownType } from 'markdown-to-jsx'
-import { WiseLink } from '@/components/wise-link'
+import MarkdownToJSX from 'markdown-to-jsx'
 import { Article } from '@/types/article'
 
 type Props = {
   readonly article: Article
 }
 
-export const Markdown: FC<Props> = ({ article }) => {
-  const overrides: MarkdownType.Overrides = {
-    h1: <h1 className="text-3xl" />,
-    h2: <h2 className="text-2xl" />,
-    img: <img className="max-w-full h-auto mb-6" />,
-    a: <WiseLink className="text-primary" />,
-    code: (
-      <code className="px-1 text-secondary text-xs bg-gray-200 rounded-sm	" />
-    ),
-  }
-
-  return (
-    <MarkdownToJSX
-      className="space-y-6 text-gray-900 leading-relaxed"
-      options={{ overrides }}
-    >
-      {article.content}
-    </MarkdownToJSX>
-  )
-}
+export const Markdown: FC<Props> = ({ article }) => (
+  <MarkdownToJSX className="prose ">{article.content}</MarkdownToJSX>
+)
